73614fea15bcc0207a0c7c67c828a5030ab2e644
Tristan Ross <tristan.ross@midstall.com>
2022-05-08 21:52:56 -0700

    fix: finally builds correctly

---
+++ build.config.ts
@@ -0,0 +1,14 @@
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig({
+  declaration: true,
+  entries: [
+    'src/module',
+    {
+      input: 'src/runtime/',
+      outDir: 'dist/runtime',
+      format: 'esm',
+      declaration: true,
+    },
+  ],
+})
